92f105d33a7931583134216cd42e7a148a4c0669
konstantinovcharov <konstantinovcharov@abv.bg>
2020-11-01 16:07:05 +0100

    EF context

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using Npgsql;
 
 namespace EntityFrameworkTest
 {
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Name = {Name}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string connectionString = "host=localhost;db=Northwind;uid=postgres;pwd=postgres;";
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = "select categoryid, categoryname from categories";
+
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var category = new Category
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1)
+                };
+
+                Console.WriteLine(category);
+                //Console.WriteLine($"Id={reader.GetInt32(0)}, Name={reader.GetString(1)}");
+            }
+
+            //Console.WriteLine("Hello World!");
         }
     }
 }
